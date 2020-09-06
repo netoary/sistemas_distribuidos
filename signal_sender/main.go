51c1bbe83f53a3f552331f6a448d954d2017ad6c
Renan <renanneri@poli.ufrj.br>
2020-09-06 12:46:18 -0300

    add signal handler

---
+++ signal_sender/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"syscall"
+)
+
+func help() {
+	fmt.Println("Este programa recebe como parâmetros o número do processo destino e o sinal que deve ser enviado")
+}
+func main() {
+
+	if len(os.Args) != 3 {
+		help()
+		os.Exit(3)
+	}
+
+	pid, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		help()
+		os.Exit(3)
+	}
+
+	signal := os.Args[2]
+	if err != nil {
+		help()
+		os.Exit(3)
+	}
+
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		help()
+		os.Exit(3)
+	}
+
+	err = syscall.Kill(pid, syscall.Signal(signal))
+	//	if(syscall.Kill(pid)){
+	//       help();
+	//        os.Exit(3);
+	//   }
+
+	fmt.Println(process)
+	fmt.Println(err)
+	fmt.Println(pid)
+	fmt.Println(signal)
+
+	fmt.Println("Enviado com sucesso")
+}
+
+//go build envia_sinal.go
+//./envia_sinal a b c d
