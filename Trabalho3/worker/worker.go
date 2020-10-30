962fdf07dc37be27b1919707567d72b38c073025
Renan <renanneri@poli.ufrj.br>
2020-10-30 18:31:47 -0300

    Initial idea for mutual exclusion, with bugs

--- Trabalho3/worker/worker.go
+++ Trabalho3/worker/worker.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
 	"os"
+	"strconv"
+	"strings"
 	"time"
-
-	"../connection"
 )
+
+var process int
+var port int
 
 func check(e error) {
 	if e != nil {
@@ -14,22 +19,80 @@ func check(e error) {
 	}
 }
 
-func write() {
-	connection.StartServerMode()
+func requestGrant(conn net.Conn) {
+	var processString string = strconv.Itoa(process)
+	// ask for access
+	var message string = "1|" + processString + "\n"
+	fmt.Fprintf(conn, message)
 
-	f, err := os.Create("./resultado.txt")
-	check(err)
+	myTurn := false
 
-	defer f.Close()
+	for !myTurn {
+		message, _ := bufio.NewReader(conn).ReadString('\n')
 
-	txt, err := f.WriteString(time.Now().Format("00:00:00.0000"))
-	check(err)
-	fmt.Printf("%d", txt)
+		if string(message[0]) == "2" && string(message[2]) == processString {
+			myTurn = true
+		}
 
-	f.Close()
+	}
+
+}
+
+func releaseGrant(conn net.Conn) {
+
+	// release grant
+	var message string = "3|" + strconv.Itoa(process) + "\n"
+	fmt.Fprintf(conn, message)
+
+}
+
+func write(conn net.Conn) {
+
+	for {
+		requestGrant(conn)
+		f, err := os.Create("./resultado.txt")
+		check(err)
+
+		txt, err := f.WriteString(time.Now().Format("00:00:00.0000"))
+		check(err)
+		fmt.Printf("%d", txt)
+		f.Close()
+
+		releaseGrant(conn)
+
+		time.Sleep(5 * time.Second)
+	}
 
 }
 
 func main() {
-	write()
+
+	// connect to server
+	conn1, err := net.Dial("tcp", "127.0.0.1:8000")
+
+	fmt.Println("Waiting...")
+	check(err)
+
+	// ask for id
+	fmt.Fprintf(conn1, strconv.Itoa(1)+"\n")
+
+	// wait id
+	message, _ := bufio.NewReader(conn1).ReadString('\n')
+	fmt.Print("Message Received: ", message)
+
+	message = strings.Replace(message, "\n", "", 1)
+	// store id
+
+	pr, _ := strconv.Atoi(message)
+	conn1.Close()
+
+	fmt.Print(process)
+	port = 8000 + int(pr)
+
+	fmt.Println(int(pr))
+	fmt.Println(port)
+	conn, err := net.Dial("tcp", "127.0.0.1:"+strconv.Itoa(port))
+
+	write(conn)
+
 }
